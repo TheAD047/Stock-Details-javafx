e1ce54f5d20f884dfdf7004c9c524aa4fc748e19
TheAD047 <200489790@student.georgianc.on.ca>
2022-11-27 15:34:36 -0500

    css

--- src/main/java/com/example/assignment2gc200489790/HelloApplication.java
+++ src/main/java/com/example/assignment2gc200489790/HelloApplication.java
@@ -21,7 +21,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
 
-        //System.out.print(APIUtil.addSymbol("MSI"));
-        //System.out.print(APIUtil.getStocks(""));
     }
 }
